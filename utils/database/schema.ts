794cd72c29526de1227b229135bc21ad1568041d
pawlurking <pawlurking@gmail.com>
2024-12-28 13:23:17 +0700

    update: [200~update: dbschema

--- utils/database/schema.ts
+++ utils/database/schema.ts
@@ -55,5 +55,37 @@ export const RewardTable = pgTable('rewards', {
 
 export const CollectedWasteTable = pgTable('collected_waste', {
   id: serial('id').primaryKey(),
-  
+  // userID foreign key for report table
+  reportID: integer('report_id').references( ()=> ReportTable.id).notNull(),
+
+  // collectorID foreign key for user table, this is needed because we use to reward to user (garbage collector)
+  collectorID: integer('collector_id').references(() => UserTable.id).notNull(),
+  collectionDate: timestamp('collection_date').notNull(),
+  // to update the status of the report to collected after everything is done !
+  status: varchar('status', {length: 255}).notNull().default('collected'),
+
+})
+
+// 
+
+export const NotificationTable = pgTable('notifications', {
+  id: serial('id').primaryKey(),
+  userID: integer('user_id').references(() => UserTable.id).notNull(),
+  message: text('message').notNull(),
+  // notification types: report award/ collect award..
+  type: varchar('type', {length: 50}).notNull(),
+  isRead: boolean('is_read').notNull().default(false),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+
+
+})
+
+export const TransactionTable = pgTable('transactions', {
+  id: serial('id').primaryKey(),
+  userID: integer('user_id').references(() => UserTable.id).notNull(),
+  // type of transaction: whether the transaction is ended or it's a redeemed transaction
+  type: varchar('type', {length: 20}).notNull(),
+
+
+
 })
